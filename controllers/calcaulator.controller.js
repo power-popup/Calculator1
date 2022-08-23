a9181b38d79d5d0e966d60e37ff1eb6f3596fed0
power-popup <82285392+power-popup@users.noreply.github.com>
2022-08-23 09:50:07 +0300

    Update calcaulator.controller.js

--- controllers/calcaulator.controller.js
+++ controllers/calcaulator.controller.js
@@ -4,6 +4,7 @@ var calcaulatorService = require('../services/calculate.service');
 
 exports.calculate = async (req, res) => {
     const { number1, number2, operator } = req.query;
+    // So when calculator object is created and initialized i can only calculate particular numbers ?
     let calcaulator = new Calcaulator(parseInt(number1),parseInt(number2),operator);
     const result = calcaulator.getResult();
     console.log(calcaulator)
