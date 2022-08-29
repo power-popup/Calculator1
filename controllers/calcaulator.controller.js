be5a0649c80d880c8aa33b21f3ffcc6c2f93d68d
power-popup <82285392+power-popup@users.noreply.github.com>
2022-08-29 13:17:44 +0300

    Update calcaulator.controller.js

--- controllers/calcaulator.controller.js
+++ controllers/calcaulator.controller.js
@@ -5,6 +5,7 @@ var calcaulatorService = require('../services/calculate.service');
 //this function get 2 paramteres and operator and calculate the result.
 exports.calculate = async (req, res) => {
     const { number1, number2, operator } = req.query;
+    //If i requested to return number of calculation results, should i create instance of Calcaulator for each one of them ?
     let calcaulator = new Calcaulator(parseInt(number1),parseInt(number2),operator);
     const result = calcaulator.getResult();
     console.log(calcaulator)
